4a35289deb008734f2ffff7ded272e88871b0a3a
CooperBraun <52896862+CooperBraun@users.noreply.github.com>
2019-10-15 12:02:58 +1100

    PauseNDeath

---
+++ UAC Project GDS210/Assets/Personal Folders/Cooper's Folder/Scripts/DeathCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathCollider : MonoBehaviour
+{
+    public GameObject DeathMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DeathMenu == true)
+        {
+            Time.timeScale = 0f;
+        }
+        if (DeathMenu == true && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DeathMenu.SetActive(true);
+        Cursor.visible = true;
+    }
+
+
+}
---
+++ UAC Project GDS210/Assets/Personal Folders/Cooper's Folder/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseParent;
+    public GameObject DeathMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseParent.SetActive(true);
+        }
+        if (PauseParent.activeInHierarchy)
+        {
+            Time.timeScale = 0f;
+        }
+        if (PauseParent.activeInHierarchy == false && DeathMenu.activeInHierarchy == false)
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
